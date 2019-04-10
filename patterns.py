7750ac9eed1c9000463dfc73bb1e6119e711dcb8
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2019-04-10 22:45:57 +0530

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -3,9 +3,10 @@ for i in range(1,6):
         print("*",end='')
     print()
 for i in range(1,6):
-    for j in range(6,i,-1):
+    for j in range(5,i-1,-1):
         print("*",end='')
     print()    
+    
 
 '''*
    **
